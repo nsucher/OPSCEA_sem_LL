edee674c6c8b69972dfc23555b4296345eab386e
nsucher <96442661+nsucher@users.noreply.github.com>
2022-06-11 11:36:55 -0700

    Delete setGlobal_ll_w_t.m

--- setGlobal_ll_w_t.m
+++
@@ -1,8 +0,0 @@
-function setGlobal_ll_w_t(ll_weight_times_in,ll_w_t_labels_in)
-global  ll_weight_times ll_w_t_labels
-
-%     Created by Natalia Sucher May 10 2022
-
-
-ll_weight_times = ll_weight_times_in;
-ll_w_t_labels = ll_w_t_labels_in;
