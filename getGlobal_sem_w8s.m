fb8357024dc1d56cdefcabe7605e89778439b4d0
nsucher <96442661+nsucher@users.noreply.github.com>
2022-06-11 11:36:22 -0700

    Delete getGlobal_sem_w8s.m

--- getGlobal_sem_w8s.m
+++
@@ -1,8 +0,0 @@
-function [LL_s_out,ytl_LL_out,yt_LL_out,u2_s_out] = getGlobal_sem_w8s
-global LL_s ytl_LL yt_LL u2_s
-
-%     Created by Natalia Sucher May 10 2022
-LL_s_out = LL_s;
-ytl_LL_out = ytl_LL;
-yt_LL_out = yt_LL;
-u2_s_out = u2_s;
