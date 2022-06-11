76a8a4db7e98c921de3108c98dac9b281b1202a1
nsucher <96442661+nsucher@users.noreply.github.com>
2022-06-11 11:36:15 -0700

    Delete getGlobal_ll_w_t.m

--- getGlobal_ll_w_t.m
+++
@@ -1,7 +0,0 @@
-function [ll_weight_times_out,ll_w_t_labels_out] = getGlobal_ll_w_t
-global ll_weight_times ll_w_t_labels
-
-%     Created by Natalia Sucher May 10 2022
-
-ll_weight_times_out = ll_weight_times;
-ll_w_t_labels_out = ll_w_t_labels;
